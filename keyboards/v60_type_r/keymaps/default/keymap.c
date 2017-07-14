e5191897898fe4d844d32f279d1c792ec3f00375
Ben Lok <lotri@users.noreply.github.com>
2017-07-13 23:46:26 -0700

    Change keymap configuration

--- keyboards/v60_type_r/keymaps/default/keymap.c
+++ keyboards/v60_type_r/keymaps/default/keymap.c
@@ -20,42 +20,42 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {    /* Keymap 0: Default Layer
      * ,-----------------------------------------------------------.
-     * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|   Bs  |
+     * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =| Bksp  |
      * |-----------------------------------------------------------|
      * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|    \|
      * |-----------------------------------------------------------|
      * |FN0   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return  |
      * |-----------------------------------------------------------|
-     * |LShift  |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /| RShift   |
+     * |LShift/(|  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /| RShift/) |
      * |-----------------------------------------------------------|
-     * |Ctrl|Alt |Gui |      Space/L3           |Gui |Alt |Ctrl|Fn1|
+     * |Ctrl|Gui |Alt |      Space/L3           |Alt |Gui |Ctrl|Fn1|
      * `-----------------------------------------------------------'
      */
     [0] = KEYMAP(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  XXXXX, KC_BSPC, \
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,     KC_BSLS, \
-        LT(1, KC_CAPS),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  \
-        KC_LSFT, XXXXX,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, XXXXX,  \
-        KC_LCTL, KC_LALT, KC_LGUI,               LT(2, KC_SPC),                          KC_RGUI, KC_RALT,  KC_RCTL, KC_FN1),
+        KC_ESC,             KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,      KC_MINS, KC_EQL,    XXXXX,                  KC_BSPC,    \
+        KC_TAB,             KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,      KC_LBRC, KC_RBRC,                           KC_BSLS,    \
+        LT(1, KC_CAPS),     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,   KC_QUOT,                                    KC_ENT,     \
+        KC_LSPO,            XXXXX,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,    KC_SLSH,            KC_RSPC,  XXXXX,                    \
+        KC_LCTL,            KC_LGUI, KC_LALT,                            LT(2, KC_SPC),                                 KC_RALT, KC_RGUI,   KC_FN1,                 KC_RCTL     ),
     /* Keymap 1: FN Layer
      * ,-----------------------------------------------------------.
      * |`  | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|  Del  |
      * |-----------------------------------------------------------|
-     * |     |   |   |   |   |   |   |   |   |   |PSc|SLk|Pau| Ins |
+     * |     |   |   |   |   |   |Cal|PgU| Up|PgD|PSc|   |   | Ins |
      * |-----------------------------------------------------------|
-     * |      |   |   |   |   |   | Lt| Dn| Up| Rt|Hom|PgU|        |
+     * |      |   |   |   |   |   |Hom| Lt| Dn| Rt|   |   |        |
      * |-----------------------------------------------------------|
-     * |        |   |   |   |   |   |Mut|VDn|VUp|End|PgD|          |
+     * |        |   |   |   |   |   |End|Mut|VDn|Vup|   |          |
      * |-----------------------------------------------------------|
      * |    |    |    |                        |    |    |    |    |
      * `-----------------------------------------------------------'
      */
     [1] = KEYMAP(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  _____,  KC_DEL, \
-        _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____, KC_PSCR, KC_SLCK, KC_PAUS,          KC_INS, \
-        _____,   _____,   _____,   _____,   _____,   _____, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_HOME, KC_PGUP,           _____,          \
-        _____,   _____,   _____,   _____,   _____,   _____,   _____, KC_MUTE, KC_VOLD, KC_VOLU,  KC_END, KC_PGDN,           _____,  _____,  \
-        _____,   _____,   _____,               _____,                                                      _____,   _____,  _____,  _____),
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,     _____,  KC_DEL, \
+        _____,   _____,   _____,   _____,   _____,   _____,   KC_CALC,  KC_PGUP,  KC_UP,    KC_PGDN,  KC_PSCR,  _____,      _____,              KC_INS,  \
+        _____,   _____,   _____,   _____,   _____,   _____,   KC_HOME,  KC_LEFT,  KC_DOWN,  KC_RGHT,  _____,    _____,                          _____,  \
+        _____,   _____,   _____,   _____,   _____,   _____,   _____,    KC_END,   KC_MUTE,  KC_VOLD,  KC_VOLU,  _____,                  _____,  _____,  \
+        _____,   _____,   _____,                              _____,                                            _____,      _____,      _____,  _____   ),
     /* Keymap 2: Mouse Keys Layer
      * ,-----------------------------------------------------------.
      * |   |   |   |   |   |   |   |   |   |   |   |   |   |       |
@@ -70,11 +70,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {    /* Keymap 0: D
      * `-----------------------------------------------------------'
      */
     [2] = KEYMAP(
-        _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____,  _____,  _____,  _____,  _____,  _____, \
-        _____,   _____,   _____,   _____,   _____,   _____,   _____,   KC_MS_WH_UP,   KC_MS_WH_DOWN,   _____, _____, _____, _____,          _____, \
-        _____,   _____,   _____,   _____,   _____,   _____, KC_MS_LEFT, KC_MS_DOWN,   KC_MS_UP, KC_MS_RIGHT, _____, _____,           _____,          \
-        _____,   _____,   _____,   _____,   _____,   _____,   KC_SPC, KC_MS_BTN1, KC_MS_BTN2, KC_MS_BTN3,  _____, _____,           _____,  _____,  \
-        _____,   _____,   _____,               _____,                                                      _____,   _____,  _____,  _____),
+        _____,   _____,   _____,   _____,   _____,   _____,   _____,        _____,          _____,      _____,          _____,      _____,  _____,  _____,  _____,  \
+        _____,   _____,   _____,   _____,   _____,   _____,   _____,        KC_MS_BTN1,     KC_MS_UP,   KC_MS_BTN2,     KC_MS_BTN3, _____,  _____,          _____,  \
+        _____,   _____,   _____,   _____,   _____,   _____,   KC_MS_WH_UP,  KC_MS_LEFT,     KC_MS_DOWN, KC_MS_RIGHT,    _____,      _____,                  _____,  \
+        _____,   _____,   _____,   _____,   _____,   _____,   _____,        KC_MS_WH_DOWN,  _____,      _____,          _____,      _____,          _____,  _____,  \
+        _____,   _____,   _____,                              _____,                                                                _____,   _____, _____,  _____   )
 };
 
 const uint16_t PROGMEM fn_actions[] = {
