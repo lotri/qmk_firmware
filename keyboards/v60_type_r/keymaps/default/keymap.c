9b122b3a1ba84760591a03415e29f2e1ab150566
Ben Lok <lotri@users.noreply.github.com>
2017-08-15 22:26:51 -0700

    Remove space cadet shift

--- keyboards/v60_type_r/keymaps/default/keymap.c
+++ keyboards/v60_type_r/keymaps/default/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {    /* Keymap 0: D
      * |-----------------------------------------------------------|
      * |FN1   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return  |
      * |-----------------------------------------------------------|
-     * |LShift/(|  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /| RShift/) |
+     * |LShift  |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /| RShift   |
      * |-----------------------------------------------------------|
      * |Ctrl|Gui |Alt |      Space/L3           |FN1 |Gui |App|Ctrl|
      * `-----------------------------------------------------------'
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {    /* Keymap 0: D
         KC_ESC,     KC_1,    KC_2,          KC_3,       KC_4,   KC_5,    KC_6,      KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,     XXXXX,      KC_BSPC,                \
         KC_TAB,     KC_Q,    KC_W,          KC_E,       KC_R,   KC_T,    KC_Y,      KC_U,       KC_I,       KC_O,       KC_P,       KC_LBRC,    KC_RBRC,                KC_BSLS,                \
         MO(1),      KC_A,    KC_S,          KC_D,       KC_F,   KC_G,    KC_H,      KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,                            KC_ENT,                 \
-        KC_LSPO,    XXXXX,   KC_Z,          KC_X,       KC_C,   KC_V,    KC_B,      KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,                KC_RSPC,    XXXXX,                  \
+        KC_LSFT,    XXXXX,   KC_Z,          KC_X,       KC_C,   KC_V,    KC_B,      KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,                KC_RSFT,    XXXXX,                  \
         KC_LCTL,    KC_LGUI, KC_LALT,                           LT(2, KC_SPC),                                                      MO(1),      KC_RGUI,    KC_APP,     MT(MOD_RCTL, KC_ENT)    ),
     /* Keymap 1: FN Layer
      * ,-----------------------------------------------------------.
